63efee71e667cc3c260ff6e21ae76c61d08ed96f
Wotori Movako <wotori@localhost.local>
2022-11-24 18:27:42 +0300

    updates

--- ui/pages/zkappWorkerClient.ts
+++ ui/pages/zkappWorkerClient.ts
@@ -1,56 +1,62 @@
-import {
-  fetchAccount,
-  PublicKey,
-  PrivateKey,
-  Field,
-} from 'snarkyjs'
+import { fetchAccount, PublicKey, PrivateKey, Field } from "snarkyjs";
 
-import type { ZkappWorkerRequest, ZkappWorkerReponse, WorkerFunctions } from './zkappWorker';
+import type {
+  ZkappWorkerRequest,
+  ZkappWorkerReponse,
+  WorkerFunctions,
+} from "./zkappWorker";
 
 export default class ZkappWorkerClient {
-
   // ---------------------------------------------------------------------------------------
 
   loadSnarkyJS() {
-    return this._call('loadSnarkyJS', {});
+    return this._call("loadSnarkyJS", {});
   }
 
   setActiveInstanceToBerkeley() {
-    return this._call('setActiveInstanceToBerkeley', {});
+    return this._call("setActiveInstanceToBerkeley", {});
   }
 
   loadContract() {
-    return this._call('loadContract', {});
+    return this._call("loadContract", {});
   }
 
   compileContract() {
-    return this._call('compileContract', {});
+    return this._call("compileContract", {});
   }
 
-  fetchAccount({ publicKey }: { publicKey: PublicKey }): ReturnType<typeof fetchAccount> {
-    const result = this._call('fetchAccount', { publicKey58: publicKey.toBase58() });
-    return (result as ReturnType<typeof fetchAccount>);
+  fetchAccount({
+    publicKey,
+  }: {
+    publicKey: PublicKey;
+  }): ReturnType<typeof fetchAccount> {
+    const result = this._call("fetchAccount", {
+      publicKey58: publicKey.toBase58(),
+    });
+    return result as ReturnType<typeof fetchAccount>;
   }
 
   initZkappInstance(publicKey: PublicKey) {
-    return this._call('initZkappInstance', { publicKey58: publicKey.toBase58() });
+    return this._call("initZkappInstance", {
+      publicKey58: publicKey.toBase58(),
+    });
   }
 
   async getNum(): Promise<Field> {
-    const result = await this._call('getNum', {});
+    const result = await this._call("getNum", {});
     return Field.fromJSON(JSON.parse(result as string));
   }
 
   createUpdateTransaction() {
-    return this._call('createUpdateTransaction', {});
+    return this._call("createUpdateTransaction", {});
   }
 
   proveUpdateTransaction() {
-    return this._call('proveUpdateTransaction', {});
+    return this._call("proveUpdateTransaction", {});
   }
 
   async getTransactionJSON() {
-    const result = await this._call('getTransactionJSON', {});
+    const result = await this._call("getTransactionJSON", {});
     return result;
   }
 
@@ -58,12 +64,14 @@ export default class ZkappWorkerClient {
 
   worker: Worker;
 
-  promises: { [id: number]: { resolve: (res: any) => void, reject: (err: any) => void } };
+  promises: {
+    [id: number]: { resolve: (res: any) => void; reject: (err: any) => void };
+  };
 
   nextId: number;
 
   constructor() {
-    this.worker = new Worker(new URL('./zkappWorker.ts', import.meta.url))
+    this.worker = new Worker(new URL("./zkappWorker.ts", import.meta.url));
     this.promises = {};
     this.nextId = 0;
 
@@ -75,7 +83,7 @@ export default class ZkappWorkerClient {
 
   _call(fn: WorkerFunctions, args: any) {
     return new Promise((resolve, reject) => {
-      this.promises[this.nextId] = { resolve, reject }
+      this.promises[this.nextId] = { resolve, reject };
 
       const message: ZkappWorkerRequest = {
         id: this.nextId,
